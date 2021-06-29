b121626969995cd6421281c76ed9cf8ccc262048
keggre <kegocandy@gmai.com>
2021-06-28 23:30:49 -0400

    fix

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ numberOfIdentities = 100
 
 chanceOfGlasses = 0                 #default 0
 useLastNameAsNameSound = False      #default false
-pitchRange = 1                      #default 1
+pitchRange = .5                      #default .5
